c69b6aedc129fb20ba85c70288549b8112d6faf2
lht <616441509@qq.com>
2026-01-03 14:21:28 +0800

    feat:init

---
+++ src/components/threeMaterial.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { startRender } from './utils.js';
+import { onMounted } from 'vue';
+onMounted(() => {
+  startRender();
+});
+</script>
+<template>
+  <div id="container">
+    <slot name="default" />
+  </div>
+</template>
+<style scoped>
+#container {
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/utils.d.ts
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+export declare function startRender(): void;
+export declare function render(): void;
+export declare let scene: THREE.Scene;
+export declare let camera: THREE.PerspectiveCamera;
+export declare let renderer: THREE.WebGLRenderer;
